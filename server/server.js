0174d2e57d690e831f08f749be810ae8f8bcb31f
root <root@DESKTOP-819667M.localdomain>
2020-05-17 17:04:48 +0600

    first commit

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
-
+import axios from 'axios'
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
 
@@ -13,6 +13,8 @@ let connections = []
 const port = process.env.PORT || 3000
 const server = express()
 
+const { readFile, writeFile, unlink } = require('fs').promises
+
 server.use(cors())
 
 server.use(express.static(path.resolve(__dirname, '../dist/assets')))
@@ -20,6 +22,59 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit
 server.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 server.use(cookieParser())
+server.use((req, res, next) => {
+  res.set('x-skillcrucial-user', '7db94c19-07d8-4b07-82e3-dbb9e1fb3f31')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  next()
+})
+const saveFile = async (users) => {
+  const b = await writeFile(`${__dirname}/primer.json`, JSON.stringify(users), { encoding: 'utf8' })
+  return b
+}
+const readUsers = async () => {
+  const a = await readFile(`${__dirname}/primer.json`, { encoding: 'utf8' })
+    .then((data) => {
+      return JSON.parse(data)
+    })
+    .catch(async () => {
+      const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+      await saveFile(users)
+      return users
+    })
+  return a
+}
+server.get('/api/v1/users', async (req, res) => {
+  const users = await readUsers()
+  res.json(users)
+})
+server.post('/api/v1/users', async (req, res) => {
+  const users = await readUsers()
+  const newUserBody = req.body
+  const UserLength = users[users.length - 1].id
+  newUserBody.id = UserLength + 1
+  const newUser = [...users, newUserBody]
+  saveFile(newUser)
+  res.json({ status: 'success', id: newUserBody.id })
+})
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const users = await readUsers()
+  const { userId } = req.params
+  const newUserBody = req.body
+  const newUserArray = users.map((it) => (it.id === +userId ? Object.assign(it, newUserBody) : it))
+  saveFile(newUserArray)
+  res.json({ status: 'success', id: userId })
+})
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const users = await readUsers()
+  const { userId } = req.params
+  users.splice(Number(userId - 1, 1))
+  saveFile(users)
+  res.json({ status: 'success', id: Number(userId) })
+})
+server.delete('/api/v1/users', async (req, res) => {
+  unlink(`${__dirname}/primer.json`)
+  res.json('ok')
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
